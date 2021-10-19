c8ffdb3fa11fb78e6edee9f9c49268a8778eba2c
Rafael <rafael_9122@yahoo.com>
2021-10-19 13:05:54 +0300

    Added model interfaces.

---
+++ KarpaticaTravel/frontend/src/app/models/city.ts
@@ -0,0 +1,6 @@
+export interface City {
+  _cityId: number
+  _countryId: number
+  name: string
+  description: string
+}
---
+++ KarpaticaTravel/frontend/src/app/models/country.ts
@@ -0,0 +1,5 @@
+export interface Country {
+  _countryId: number
+  countryCode: string
+  name: string
+}
