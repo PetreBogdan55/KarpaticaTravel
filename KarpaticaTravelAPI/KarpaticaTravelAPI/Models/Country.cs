b0a2bc79521ba9239debf5289a2188be810f3a1b
Alex N <alexneacsu2000@gmail.com>
2021-10-22 18:17:28 +0300

    Added CountriesController to perform CRUD operations on Country table.

---
+++ KarpaticaTravelAPI/KarpaticaTravelAPI/Models/Country.cs
@@ -0,0 +1,12 @@
+
+namespace KarpaticaTravelAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string Name { get; set; }
+    }
+}
