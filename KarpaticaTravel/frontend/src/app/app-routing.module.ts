6813628b1a8603750713ebdb19e53545d302e30d
Rafael <rafael_9122@yahoo.com>
2021-10-26 12:33:38 +0300

    Completed folder structure & basic routing.

--- KarpaticaTravel/frontend/src/app/app-routing.module.ts
+++ KarpaticaTravel/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { LoginComponent } from './components/login/login.component'
+import { StartComponent } from './components/start/start.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'start' },
+  { path: 'login', component: LoginComponent },
+  { path: 'start', component: StartComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- KarpaticaTravel/frontend/src/app/app.module.ts
+++ KarpaticaTravel/frontend/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { LoginComponent } from './components/login/login/login.component'
-import { StartComponent } from './components/start/start/start.component'
+import { LoginComponent } from './components/login/login.component'
+import { StartComponent } from './components/start/start.component'
 
 @NgModule({
   declarations: [AppComponent, StartComponent, LoginComponent],
