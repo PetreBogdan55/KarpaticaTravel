5ab96014a56177ef653b0c21879c64970e00d61a
Rafael <rafael_9122@yahoo.com>
2021-10-26 12:26:31 +0300

    Added Login & Start components & shared service & module.

--- KarpaticaTravel/frontend/src/app/app.module.ts
+++ KarpaticaTravel/frontend/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { LoginComponent } from './components/login/login/login.component'
+import { StartComponent } from './components/start/start/start.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, StartComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
