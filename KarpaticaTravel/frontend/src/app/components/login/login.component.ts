46be7741e73b93934f7f7eff6ab1b859ad1a909b
Rafael <rafael_9122@yahoo.com>
2021-10-26 13:57:03 +0300

    Module refactoring.

--- KarpaticaTravel/frontend/src/app/components/login/login.component.ts
+++ KarpaticaTravel/frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
-import { NgForm } from '@angular/forms'
 import { HttpClient } from '@angular/common/http'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
