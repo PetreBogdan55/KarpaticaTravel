48fd82ab856584b2e07e932b283798bae405343e
worstcodah <rafael4296@gmail.com>
2021-10-31 18:28:44 +0200

    Added button which redirects to login in register component.

--- KarpaticaTravel/frontend/src/app/components/register/register.component.ts
+++ KarpaticaTravel/frontend/src/app/components/register/register.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core'
-import { NgForm } from '@angular/forms'
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
-import { Router } from '@angular/router'
-import { NgModel } from '@angular/forms'
-import { environment } from 'src/environments/environment'
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgModel } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,8 +11,8 @@ import { environment } from 'src/environments/environment'
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  public signupForm!: FormGroup
-  siteKey: string = environment.siteKey
+  public signupForm!: FormGroup;
+  siteKey: string = environment.siteKey;
 
   constructor(private formBuilder: FormBuilder, private router: Router) {
     this.signupForm = this.formBuilder.group({
@@ -20,10 +20,12 @@ export class RegisterComponent implements OnInit {
       password: ['', [Validators.minLength(5), Validators.maxLength(15)]],
       username: ['', [Validators.minLength(5), Validators.maxLength(15)]],
       recaptcha: ['', Validators.required],
-    })
+    });
   }
 
   ngOnInit() {}
 
-  signup(): void {}
+  register(form: FormGroup): void {
+    console.log(form.value);
+  }
 }
