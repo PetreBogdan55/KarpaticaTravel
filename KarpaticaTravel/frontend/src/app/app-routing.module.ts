e2241079b5e7b7594da21d432a35c689951409fc
Petre Mihai Bogdan <petrebogdan55@yahoo.com>
2021-11-01 20:54:08 +0200

    Added "Search for packages" feature in Start Page

--- KarpaticaTravel/frontend/src/app/app-routing.module.ts
+++ KarpaticaTravel/frontend/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { LoginComponent } from './components/login/login.component'
-import { StartComponent } from './components/start/start.component'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { StartComponent } from './components/start/start.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'start' },
+  { path: '', redirectTo: 'start', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'start', component: StartComponent },
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
