b8e0832e614afc2aead93574ad7ac03155a65e8d
Rafael <rafael_9122@yahoo.com>
2021-11-02 13:58:35 +0200

    Added error validation messages.

--- KarpaticaTravel/frontend/src/app/components/register/register.component.ts
+++ KarpaticaTravel/frontend/src/app/components/register/register.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { NgModel } from '@angular/forms';
-import { environment } from 'src/environments/environment';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
+import { Router } from '@angular/router'
+import { NgModel } from '@angular/forms'
+import { environment } from 'src/environments/environment'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -11,21 +11,39 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  public signupForm!: FormGroup;
-  siteKey: string = environment.siteKey;
+  public signupForm!: FormGroup
+  siteKey: string = environment.siteKey
 
   constructor(private formBuilder: FormBuilder, private router: Router) {
-    this.signupForm = this.formBuilder.group({
-      email: ['', Validators.email],
-      password: ['', [Validators.minLength(5), Validators.maxLength(15)]],
-      username: ['', [Validators.minLength(5), Validators.maxLength(15)]],
-      recaptcha: ['', Validators.required],
-    });
+    this.signupForm = this.formBuilder.group(
+      {
+        email: ['', Validators.required],
+        password: ['', [Validators.minLength(5), Validators.maxLength(15)]],
+        username: ['', [Validators.minLength(5), Validators.maxLength(15)]],
+        recaptcha: ['', Validators.required],
+      },
+      { updateOn: 'change' },
+    )
   }
 
-  ngOnInit() {}
+  ngOnInit(): void {}
+
+  getUsername(): FormControl {
+    return this.signupForm.get('username') as FormControl
+  }
+
+  isInvalid(form: FormControl): boolean {
+    return form.invalid && (form.dirty || form.touched)
+  }
+
+  getPassword(): FormControl {
+    return this.signupForm.get('password') as FormControl
+  }
+  getEmail(): FormControl {
+    return this.signupForm.get('email') as FormControl
+  }
 
   register(form: FormGroup): void {
-    console.log(form.value);
+    console.log(form.value)
   }
 }
