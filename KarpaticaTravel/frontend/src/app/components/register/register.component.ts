3d6341d68e72a9748400daeb2cbca2bd9c1b46c6
worstcodah <rafael4296@gmail.com>
2021-12-26 15:43:19 +0200

    Fixed email validator bug.

--- KarpaticaTravel/frontend/src/app/components/register/register.component.ts
+++ KarpaticaTravel/frontend/src/app/components/register/register.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
-import { Router } from '@angular/router'
-import { NgModel } from '@angular/forms'
-import { environment } from 'src/environments/environment'
-import { SharedFormService } from 'src/app/shared/services/shared.form.service'
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgModel } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { SharedFormService } from 'src/app/shared/services/shared.form.service';
 
 @Component({
   selector: 'app-register',
@@ -11,27 +16,33 @@ import { SharedFormService } from 'src/app/shared/services/shared.form.service'
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  siteKey: string = environment.siteKey
+  siteKey: string = environment.siteKey;
 
   constructor(
     public formService: SharedFormService,
     private formBuilder: FormBuilder,
-    private router: Router,
+    private router: Router
   ) {
     this.formService.form = this.formBuilder.group(
       {
-        email: ['', Validators.required],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(this.formService.emailPattern),
+          ],
+        ],
         password: ['', [Validators.minLength(5), Validators.maxLength(15)]],
         username: ['', [Validators.minLength(5), Validators.maxLength(15)]],
         recaptcha: ['', Validators.required],
       },
-      { updateOn: 'change' },
-    )
+      { updateOn: 'change' }
+    );
   }
 
   ngOnInit(): void {}
 
   register(form: FormGroup): void {
-    console.log(form.value)
+    console.log(form.value);
   }
 }
