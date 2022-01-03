4fa4513d7aa80f9fbc1c7bdaa7843381312e5ad0
Mihai Cobzaru <mishuc04@gmail.com>
2022-01-03 17:37:52 +0200

    Added BookingRepository and IBookingRepository interface

---
+++ KarpaticaTravelAPI/KarpaticaTravelAPI/Repositories/BookingRepository/IBookingRepository.cs
@@ -0,0 +1,17 @@
+﻿using KarpaticaTravelAPI.Models.BookingModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KarpaticaTravelAPI.Repositories.BookingRepository
+{
+    interface IBookingRepository
+    {
+        Task<bool> CreateBooking(Booking booking);
+        Task<bool> UpdateBooking(Guid id, Booking booking);
+        Task<bool> DeleteBooking(Guid id);
+        Task<Booking> GetBooking(Guid id);
+        Task<IEnumerable<Booking>> GetBookings();
+    }
+}
